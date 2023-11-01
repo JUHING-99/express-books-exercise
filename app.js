433a5f55502692a3f14bd94bb7143d0f161655de
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-01 10:53:16 +0100

    al tests passed

--- app.js
+++ app.js
@@ -17,15 +17,36 @@ app.get('/all', (req, res) => {
     res.json(books[books.length - 1])
   })
 
-
-
-
-
-
-
-
-
   
+  app.get('/middle', (req, res) => {
+    res.json(books[books.length/2])
+  })
+
+  app.get('/author/dante-alighieri', (req, res) => {
+    res.json(books.filter(book => book.author==="Dante Alighieri")[0].title)
+  })
+
+  app.get('/country/charles-dickens', (req, res) => {
+    res.json(books.filter(book => book.author==="Charles Dickens")[0].country)
+  })
+
+  app.get('/year&pages/cervantes', (req, res) => {
+    res.json(books.filter(book => book.author==="Miguel de Cervantes").map(book => {return {"pages": book.pages, "year": book.year}})[0])
+  })
+ 
+  app.get('/country/count/spain', (req, res) => {
+    res.json(books.filter(book => book.country==="Spain").length)
+  })
+
+  app.get('/country/at-least/germany', (req, res) => {
+    res.json(books.filter(book => book.country==="Germany").length>0)
+  })
+
+
+  app.get('/pages/all-greater/200', (req, res) => {
+    res.json(books.every(book => book.pages>200))
+  })
+
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
 });
