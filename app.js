2b635d80a055174065c6a867d933827f2aa9b7ab
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-01 10:21:12 +0100

    all books

---
+++ app.js
@@ -0,0 +1,18 @@
+const books = require('./data/books.json');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/all', (req, res) => {
+    res.json(books)
+  })
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+});
